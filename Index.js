2129f4d436f63abd264629a83dd88e562975343c
Gods-Blessing <mohitadhana2569@gmail.com>
2022-09-19 21:19:01 +0530

    added delete btn for every task and added task using js

--- Index.js
+++ Index.js
@@ -40,13 +40,38 @@ let alarmcontainer = document.getElementById('alarm-container');
 
 
 setalarmbutton.addEventListener('click', function(){
+    // made a new new elemnt div
     let div = document.createElement('div');
-    let test = document.createTextNode("hii help me");
+    
+    // adding styling to div
+    div.style.width = "100%";
+    div.style.height = "2em";
+    div.style.border = "1px solid brown";
+    div.style.display = "flex";
+    div.style.justifyContent = "space-between";
+    div.style.alignItems = "center";
 
+
+    // adding id to the divs
     div.id = document.getElementById('hours').value +":" + document.getElementById('minutes').value+":"+document.getElementById("seconds").value+":"+document.getElementById('ampm').value;
 
-    console.log(document.getElementById('hours').value +":" + document.getElementById('minutes').value+":"+document.getElementById("seconds").value+":"+document.getElementById('ampm').value);
-    div.appendChild(test)
+    // creating p for div
+    let p = document.createElement('p');
+    p.style.marginLeft = "5%";
+    p.innerText = document.getElementById('hours').value +":" + document.getElementById('minutes').value+":"+document.getElementById("seconds").value+":"+document.getElementById('ampm').value;
+     div.appendChild(p);
+
+    // adding delete button for divs
+    let delbtn = document.createElement('div');
+    delbtn.style.width = "25%";
+    delbtn.style.height = "70%";
+    delbtn.style.border = "1px solid green";
+    delbtn.innerText = "DELETE";
+    delbtn.style.marginRight = "5%";
+
+    div.appendChild(delbtn);
+
+    // div.appendChild(test)
     alarmcontainer.appendChild(div);
 })
 
